8f967295cc748f6fd11ff0712f37f882c6a00e00
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-24 19:13:53 -0300

    feat: add boolean studies in typescript

---
+++ Modulo-02/10-boolean/booleanType.js
@@ -0,0 +1,17 @@
+/*
+Nome: Diego Ferreira Crivelaro
+GitHub: https://github.com/diegocrivelaro
+*/
+// Boolean != boolean (boolean é um tipo primitivo, enquanto que o Boolean é um object)
+
+// Exemplos 1
+var tarefaPendente = false;
+console.log(tarefaPendente);
+
+// Exemplos 2
+var tarefaConcluida = true;
+if (tarefaConcluida) {
+  console.log("Tarefa concluída!");
+} else {
+  console.log("Tarefa pendente!");
+}
---
+++ Modulo-02/10-boolean/booleanType.ts
@@ -0,0 +1,18 @@
+/*
+Nome: Diego Ferreira Crivelaro
+GitHub: https://github.com/diegocrivelaro
+*/
+
+// Boolean != boolean (boolean é um tipo primitivo, enquanto que o Boolean é um object)
+
+// Exemplos 1
+const tarefaPendente: boolean = false;
+console.log(tarefaPendente);
+
+// Exemplos 2
+const tarefaConcluida: boolean = true;
+if (tarefaConcluida) {
+  console.log("Tarefa concluída!");
+} else {
+  console.log("Tarefa pendente!");
+}
